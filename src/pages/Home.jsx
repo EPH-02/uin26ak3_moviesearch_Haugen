5077fe870980149a0721e5612f8056b49fcd8c8a
EPH-02 <eskilhaugen@live.no>
2026-03-02 14:00:29 +0100

    hånterer lokal lagring må utvides

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import History from "../components/History"
 
 
 export default function Home(){
     const [search, setSearch] = useState()
+    const storedHistory = localStorage.getItem("search")
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+    const [focused, setFocused] = useState(false)
+
+    console.log("denne kommer fra storage", storedHistory)
+
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
     // BRA PRATICE: Legg API key i .env fil og hent den derfra, slik at den ikke ligger i koden
     const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    useEffect(()=> {
+        localStorage.setItem("search", JSON.stringify(history))
+    }),[history]
 
     const getMovies = async()=>{
         try{
@@ -25,20 +35,27 @@ export default function Home(){
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-        getMovies()
+        e.target.reset()
+       
+        setHistory((prev) => [...prev, search])
+        
+        
+         //getMovies()
     }
     
+    console.log(history)
+
     return(
         <main>
             <h1>Forside</h1>
             <form onSubmit={handleSubmit}>
                 <label>
                     Søk etter film
-                    <input type="search" placeholder="Indiana Jones" onChange={handleChange}></input>
-                </label>
+                    <input type="search" placeholder="Indiana Jones" onChange={handleChange} onFocus={()=> setFocused(true)} onBlur={()=> setFocused(false)}></input>
+                </label>   
+            {focused ? <History history={history} setSearch={setSearch} /> : null}
                 <button onClick={getMovies}>Søk</button>
             </form>
-           
         </main>
     )
 }
